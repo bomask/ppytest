a9cedc5fa74f1bb05a3c7088d1e765e6bdd32283
yangb-i <yangb-i@glodon.com>
2021-04-27 10:22:40 +0800

    编码

---
+++ Source Files/Data/LoginData.py
@@ -0,0 +1,10 @@
+# 登录模块
+__page__ = "https://mail.163.com/"
+
+# ______________登录模块______________
+# 账户输入框
+data_login_account_xpath = '//*[@id="auto-id-1619421097532"]'
+# 密码输入框
+data_login_password_xpath = '//*[@id="auto-id-1619421097535"]'
+# 登录确认按钮
+data_login_login__xpath = '// *[@id = "dologin"]'
---
+++ Source Files/Data/RegisterData.py
@@ -0,0 +1,13 @@
+# 注册模块
+__page__ = "https://mail.163.com/register/index.htm?from=163mail&utm_source=163mail"
+
+
+# ______________注册模块______________
+# 账户
+data_register_account_id = 'username'
+# 密码输入框
+data_register_password_id = 'password'
+# 手机号输入框
+data_register_phone__id = 'phone'
+# 服务条款勾选框
+data_register_service__id = 'service'
